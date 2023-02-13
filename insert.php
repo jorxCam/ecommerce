28bd2e0a9854189452d2882b915d7b0e25e261aa
jorCam <jcamacxo@gmail.com>
2023-02-13 20:23:15 +0100

    password hash added

--- insert.php
+++ insert.php
@@ -7,12 +7,23 @@ $pseudo= $_POST['pseudo'];
 $email= $_POST['email'];
 $pwd= $_POST['pwd'];
 
+
+
+//Obtenemos contraseña desde un form.
+$contrasena = $_POST['pwd'] ?: '';
+
+//Encriptamos de manera segura la contraseña
+$contrasena = password_hash($pwd, PASSWORD_DEFAULT);
+
+
 try {
 
-    $sql = "INSERT INTO user(login, password, email,nom, prenom) VALUES ('$pseudo','$pwd','$email','$lname','$fname')";
+    $sql = "INSERT INTO user(login, password, email, nom, prenom) VALUES ('$pseudo','$contrasena','$email','$lname','$fname')";
     // use exec() because no results are returned
     $conn->exec($sql);
     //echo "New record created successfully";
+    //echo '<script>alert("utilisateur creé")</script>';
+    header("Location: index.php");
 
 } catch(PDOException $e) {
     echo $sql . "<br>" . $e->getMessage();
--- login.php
+++ login.php
@@ -1,57 +1,39 @@
 <?php
 require 'bdd.php';
 
-
 session_start();
 $panier=array();;
 
 
 $login= $_POST["login"];
-$pwd= $_POST["pwd"];
+$pass= $_POST["pwd"];
+$pseudo='';
+
+//$pwd=password_hash($pass, PASSWORD_DEFAULT);
+$contrasenapost = $_POST['pwd'] ?: '';    
+
+//Sentencia SQL
+$stmt = $conn->prepare("SELECT * FROM user WHERE email = '$login'");
+$stmt->execute();  
 
 
-
-class TableRows extends RecursiveIteratorIterator {
-  function __construct($it) {     parent::__construct($it, self::LEAVES_ONLY);   }
-  function current() {     return "" . parent::current(). "";   }
-  function beginChildren() {     echo "";   }
-  function endChildren() {     echo "" . "\n";   }
-}
-
-try {
-
-    //$stmt = $conn->prepare("SELECT * FROM user  ");
-    $stmt = $conn->prepare("SELECT login FROM user WHERE email = '$login' and password ='$pwd' ");
-    $stmt->execute();
+// set the resulting array to associative
+$result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+$v='';
+//$pseudo='';
+foreach(($stmt->fetchAll()) as $k=>$v) {
+  $contrasenaDB=$v['password'];
+  $pseudo=$v['login'];
   
-    // set the resulting array to associative
-    $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
-    $v='';
-    //$pseudo='';
-    foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) {
-      //echo $v;
-      
-    }
-    $pseudo=$v;
-    //echo $v;
-
-    if ($v==true){
-        //echo ' bienvenue ' ;
-        $_SESSION['username']= $pseudo;
-        $_SESSION['panier']= $panier;
-        header("Location: accueil.php");
-        exit();
-    }else{
-        //if ($v==)
-        echo 'invalid login';
-        header("Location: index.php");
-        exit();
-       
-    }
-
-
-} catch(PDOException $e) {
-    echo $conn . "<br>erreur conection bdd <br>" . $e->getMessage();
 }
 
-//$conn = null;
+if (password_verify($contrasenapost,$contrasenaDB)) {
+  $_SESSION['username']= $pseudo;
+  $_SESSION['panier']= $panier;
+  header("Location: accueil.php");
+  exit();   
+} else { 
+  echo 'invalid login';
+  header("Location: index.php");
+  exit();
+}
